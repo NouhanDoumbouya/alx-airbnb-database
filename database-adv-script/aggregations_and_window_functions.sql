c74b5b3f83b4318a7c34c34c0382db79b8562f0e
NouhanDoumbouya <nouhandoumbouya655@gmail.com>
2025-05-18 14:08:49 +0800

    hot fix

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -7,7 +7,7 @@ GROUP BY user_id;
 SELECT 
     property_id,
     COUNT(*) AS total_bookings,
-    RANK OVER (ORDER BY COUNT(*) DESC) AS booking_rank
+    RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
     ROW_NUMBER() OVER (PARTITION BY property_id ORDER BY COUNT(*) DESC) AS booking_rank
 FROM Bookings
 GROUP BY property_id;
